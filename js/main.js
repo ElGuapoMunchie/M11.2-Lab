e80986ee4a8466475beb5ee0493fa823bad8e8cd
Mark Muench <markmuench@u.boisestate.edu>
2024-11-03 22:33:28 -0700

    Finished the Lab!! :)

--- js/main.js
+++ js/main.js
@@ -30,15 +30,21 @@ function loadData() {
     });
 
     xhr.open("GET", "https://5zol9aa2td.execute-api.us-east-2.amazonaws.com/items");
-    xhr.send;
+    xhr.send();
 }
 
 // Delete the item using DELETE
 function deleteItem(id) {
     let xhr = new XMLHttpRequest();
-    xhr.open("DELETE", "https://5zol9aa2td.execute-api.us-east-2.amazonaws.com/items" + id);
+    xhr.open("DELETE", "https://5zol9aa2td.execute-api.us-east-2.amazonaws.com/items/" + id);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send();
+}
+
+// Check if an input is a number
+function isNumber(value) {
+    // Convert the value to a number and check if it's valid
+    return !isNaN(Number(value)) && value.trim() !== "";
 }
 
 // Add the item using PUT
@@ -47,13 +53,18 @@ function addItem() {
     xhr.open("PUT", "https://5zol9aa2td.execute-api.us-east-2.amazonaws.com/items");
     xhr.setRequestHeader("Content-Type", "application/json");
 
-    var id = document.getElementById("id");
-    var name = document.getElementById("name");
-    var price = document.getElementById("price");
-    var check = document.getElementById("check");
+    var id = document.getElementById("ID");
+    var name = document.getElementById("Name");
+    var price = document.getElementById("Price");
+
+    console.log(id, name, price)
 
     // Check that input fields are valid (i.e. not NULL or empty)
-    if (id.value != "" && id.name != "" && id.price != "") {
+    if (id.value != "" && name.value != "" && price.value != "" && isNumber(price.value) && isNumber(id.value)) {
+
+        if (isNumber(price.value)) {
+
+        }
 
         xhr.send(JSON.stringify({
             "id": id.value,
@@ -61,10 +72,20 @@ function addItem() {
             "name": name.value
         }));
 
-        check.textContent = "Added Item"; // Tell user item was added
-        loadData(); // Refresh the table (because why hit load unless we have to)
-    } else {
-        check.textContent = "Cannot have empty fields. Ensure all items have a name, price, and id."
-    }
+        checkItemAdded.textContent = "Successfully Added Item"; // Tell user item was added
 
+        // Refresh the table (because why hit load unless we have to)
+        // loadData();
+
+        // Reset the form to placeholder values
+        document.getElementById("addItemForm").reset();
+
+    } else {
+        // Provide error message corresponding to problem
+        if (name === "") {
+            checkItemAdded.textContent = "Error: Cannot have empty fields. Ensure all items have a name, price, and id.";
+        } else if (!isNumber(price) || !isNumber(id)) {
+            checkItemAdded.textContent = "Error: Price and ID fields must be a number (e.g. numeric digits [0-9, etc]).";
+        }
+    }
 }
